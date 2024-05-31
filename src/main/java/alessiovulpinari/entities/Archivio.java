4b7d875387e7e35e7f90ce2316ceda65ac7e233a
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-31 12:03:55 +0200

    addToCatologue and removeCatalogueElement
    - addToCatologue: add a new catalogue element to the catalogue.
    
    - removeCatalogueElement: remove an element using his isbn code.

--- src/main/java/alessiovulpinari/entities/Archivio.java
+++ src/main/java/alessiovulpinari/entities/Archivio.java
@@ -1,6 +1,7 @@
 package alessiovulpinari.entities;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Archivio {
     List<ElementoCatalogo> catalogueList;
@@ -15,5 +16,26 @@ public class Archivio {
 
     public void setCatalogueList(List<ElementoCatalogo> catalogueList) {
         this.catalogueList = catalogueList;
+    }
+
+    public void addToCatalogueList(ElementoCatalogo elementoCatalogo) {
+        this.getCatalogueList().add(elementoCatalogo);
+        System.out.println(elementoCatalogo.getTitle() + " aggiunto con successo!");
+    }
+
+    public void removeCatalogueElement(String isbn) {
+        if (this.getCatalogueList().isEmpty()) {
+            System.out.println("Il tuo archivio è vuoto!");
+        } else {
+            ElementoCatalogo elementToRemove = this.getCatalogueList().stream().filter(elementoCatalogo -> Objects.equals(elementoCatalogo.getIsbn(), isbn)).toList().getFirst();
+
+            if (elementToRemove != null) {
+                this.getCatalogueList().remove(elementToRemove);
+                System.out.println(elementToRemove.getTitle() + " rimosso con successo!");
+            } else {
+                System.out.println("Nessun elemento con isbn: " + isbn + " è presente nella tua lista Archivio");
+            }
+
+        }
     }
 }
